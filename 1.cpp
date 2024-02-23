573aa2b3d64f888e43f662f57385ae20048156b2
Faysal Mahmud <95643831+Faysal3010@users.noreply.github.com>
2024-02-24 01:17:33 +0600

    Create 1.cpp

---
+++ 1.cpp
@@ -0,0 +1,12 @@
+/*
+Write a program that prints:
+
+“Bangladesh ”
+Hello class of cse115L!! Welcome to BD .
+*/
+#include <iostream>
+using namespace std;
+int main()
+{
+cout<<"\"Bangladesh\"\nHello class of cse115L!! Welcome to BD .";
+}
