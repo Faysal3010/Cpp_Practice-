747f3e53d5f2341e643e98187dd4489016d8999b
Faysal Mahmud <95643831+Faysal3010@users.noreply.github.com>
2024-02-24 01:49:25 +0600

    Create 7.cpp

---
+++ 7.cpp
@@ -0,0 +1,43 @@
+/*
+Ask user for two integers a and b. Then swap (interchange) the values of a and b. 
+That means, a should get the value of b and b should get the value of a.
+Enter a: 7
+Enter b: 3
+After swapping, a: 3 and b: 7
+*/
+
+#include <iostream>
+using namespace std;
+int main()
+{
+  int a, b, temp;
+  cout << "Enter a: ";
+  cin >> a;
+  cout << "Enter b: ";
+  cin >> b;
+  temp = a;
+  a = b;
+  b = temp;
+  cout << "After swapping,\n"
+       << " a: " << a << " and b: " << b << endl;
+  return 0;
+}
+
+
+// without temporary....
+#include <iostream>
+using namespace std;
+int main()
+{
+  int a, b;
+  cout << "Enter a: ";
+  cin >> a;
+  cout << "Enter b: ";
+  cin >> b;
+  a = a - b;
+  b = a + b;
+  a = b - a;
+  cout << "After swapping,\n"
+       << " a: " << a << " and b: " << b << endl;
+  return 0;
+}
