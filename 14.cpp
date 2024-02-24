4eecdb3bfa743b4b592a2b34c73cfca4acae7204
Faysal Mahmud <95643831+Faysal3010@users.noreply.github.com>
2024-02-24 11:05:49 +0600

    Create 14.cpp

---
+++ 14.cpp
@@ -0,0 +1,5 @@
+/*
+    Solve the equation: ax2+bx+c=0 and print the solutions. Read a,b,c from user
+Hint: compute the values of (-b+sqrt(b2-4ac))/2a and (-b-sqrt(b2-4ac))/2a; here sqrt is a C function that computes the squared root of a number
+
+*/
