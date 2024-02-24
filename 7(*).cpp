4edd26e69e1e0682c4245771f1dc419005951632
Faysal Mahmud <95643831+Faysal3010@users.noreply.github.com>
2024-02-24 10:14:51 +0600

    Update 7(*).cpp

--- 7(*).cpp
+++ 7(*).cpp
@@ -41,3 +41,16 @@ int main()
        << " a: " << a << " and b: " << b << endl;
   return 0;
 }
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a, b;
+    cin >> a >> b;
+    a=a+b;
+    b=a-b;
+    a=-(b-a);
+    cout<<a<<" "<<b<<endl;
+    return 0;
+}
