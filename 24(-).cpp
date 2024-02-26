48a8cae6d243916176263ab08d9f3477dc5801f7
Faysal Mahmud <95643831+Faysal3010@users.noreply.github.com>
2024-02-26 11:47:08 +0600

    Create 24(-).cpp

---
+++ 24(-).cpp
@@ -0,0 +1,11 @@
+/*  Use 1's complement and 2's complement */
+#include <iostream>
+using namespace std;
+int main()
+{
+     int a, b;
+     cout << "Enter two numbers: ";
+     cin >> a;
+     cout << "-a: " << -a << ", ~a: " << ~a;
+     return 0;
+}
