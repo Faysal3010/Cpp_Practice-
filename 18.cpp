f1a03ab70c05cbeddcbdf1ce0371d30523f2849c
Faysal Mahmud <95643831+Faysal3010@users.noreply.github.com>
2024-02-26 10:56:55 +0600

    Update 18.cpp

--- 18.cpp
+++ 18.cpp
@@ -1,1 +1,27 @@
+/*
+    Print the sum of the series:1+2+...+n; read n from user (Hint: 1+2+...+n= n(n-1)/2)
+*/
 
+#include <iostream>
+using namespace std;
+int n;
+void fx()
+{
+  for (int i = 0; i < n; i++)
+  {
+    cout << i;
+    if (i != n)
+    {
+      cout << "+";
+    }
+  }
+}
+int main()
+{
+
+  cout << "Enter the value of n: ";
+  cin >> n;
+  fx();
+  cout << n << " is: " << (n * (n + 1)) / 2;
+  return 0;
+}
