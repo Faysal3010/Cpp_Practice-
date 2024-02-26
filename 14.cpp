d8a668fd830f347b55585cc6e540980511a89079
Faysal Mahmud <95643831+Faysal3010@users.noreply.github.com>
2024-02-26 10:11:51 +0600

    Update 14.cpp

--- 14.cpp
+++ 14.cpp
@@ -3,3 +3,20 @@
 Hint: compute the values of (-b+sqrt(b2-4ac))/2a and (-b-sqrt(b2-4ac))/2a; here sqrt is a C function that computes the squared root of a number
 
 */
+
+
+
+#include <iostream>
+#include <math.h>
+using namespace std;
+int main()
+{
+  float a, b, c;
+  cout << "\tax^2+bx+c=0"
+       << "\nEnter\ta  b  c\n\t";
+  cin >> a >> b >> c;
+  cout << "value x: " << ((-b + sqrt(pow(b, 2) - 4 * a * c))/2*a)
+       << " , " << ((-b - sqrt(pow(b, 2) - 4 * a * c))/2*a) << endl;
+  return 0;
+}
+
