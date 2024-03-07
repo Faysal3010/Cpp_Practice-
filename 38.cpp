875027525ba9babeaf110745663f99b40ee4fafd
Faysal Mahmud <95643831+Faysal3010@users.noreply.github.com>
2024-03-08 03:45:18 +0600

    Create 38.cpp

---
+++ 38.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    double a, b, c;
+    while (1)
+    {
+        cout << "\nEnter the three numbers: ";
+        cin >> a >> b >> c;
+        if (a < b & a < c)
+        {
+            cout << a;
+        }
+        else if (b < c)
+        {
+            cout << b;
+        }
+        else if (a == c & b == c)
+            cout << "All equal" << endl;
+        else
+            cout << c;
+    }
+    return 0;
+}
