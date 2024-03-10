db0b452653670b82cfb9b22a69c6fa6996bfc59c
Faysal Mahmud <95643831+Faysal3010@users.noreply.github.com>
2024-03-10 20:06:34 +0600

    Create 77.cpp

---
+++ 77.cpp
@@ -0,0 +1,8 @@
+/*Write a C++ program to print all odd numbers from 1 to n (n is user input) using for loop. */
+for(i=1;  i<=n;  i++)
+    {
+        if(i%2!=0)        // Check if the number is odd
+        {
+            cout<<i<<"  ";
+        }
+    }
