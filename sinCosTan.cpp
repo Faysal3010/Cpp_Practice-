367b5cdabf180f21ba07a29b70c719997a8db894
Faysal Mahmud <95643831+Faysal3010@users.noreply.github.com>
2024-04-28 18:29:36 +0600

    Update sinCosTan.cpp

--- sinCosTan.cpp
+++ sinCosTan.cpp
@@ -7,7 +7,11 @@ int main()
     double v;
     while (cin >> v)
     {
-        cout << acos(v) * degree << endl;
-        cout << cos(v * rad) << endl;
+        cout <<"cosh: "<< acos(v) * degree << endl;
+        cout <<"cos: "<< cos(v * rad) << endl;
+        cout <<"tanh: "<< atan(v) * degree << endl;
+        cout <<"tan: "<< tan(v * rad) << endl;
+        cout <<"asin: "<< asin(v) * degree << endl;
+        cout <<"sin: "<< sin(v * rad) << endl;
     }
 }
